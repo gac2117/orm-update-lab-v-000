5bf71a27cac16357a448991c2b46e8777c01bdd3
Grace Choi <oiograceoio@gmail.com>
2019-03-21 00:52:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,12 +48,11 @@ class Student
     student
   end
 
-  def self.new_from_db(array)
-    student = self.new
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
-    student
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
