41a74d84b7bdd74819caeaafa4d1f8330433ac37
Grace Choi <oiograceoio@gmail.com>
2019-03-21 00:41:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
 
   def self.create(name, grade)
     student = self.new(name, grade)
-    student.save 
+    student.save
     student
   end
 
@@ -56,5 +56,5 @@ class Student
     student
   end
 
-  
+
 end
